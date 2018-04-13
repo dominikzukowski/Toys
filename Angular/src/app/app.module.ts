60f901a429eaef53905abfa152deca54c78d21f4
Dominik Zukowski <dominik.zukowski@onwelo.com>
2018-04-13 13:44:49 +0200

    Add toys-list component

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ToysListComponent } from './toys-list/toys-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToysListComponent
   ],
   imports: [
     BrowserModule
