a150c710933ec668422a1c0fb570759eda990e60
Dominik <dom.zukowski@gmail.com>
2018-04-15 21:14:43 +0200

    Add product

---
+++ WebApi/Model/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ToysApi.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double Rating { get; set; }
+    }
+}
---
+++ WebApi/ToysApi.Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ToysApi.Model;
+
+namespace ToysApi.Repository
+{
+    public class ProductRepository
+    {
+        private IEnumerable<Product> products = new List<Product>() {
+            new Product(){ Id = 1, Name = "Teady bear", Description = "Big teady bear", Price = 24, Rating = 4.5 },
+            new Product(){ Id = 2, Name = "Bricks", Description = "200 pieces", Price = 30, Rating = 3.5 },
+            new Product(){ Id = 3, Name = "Doll", Description = "Small, pink dall", Price = 15, Rating = 4.2 }};
+
+        public Product Get(int id)
+        {
+            return products.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+    } 
+}
